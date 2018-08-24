144e5f7a17fd69f25627d874fcb35b94913e33b8
cherucole <cheruyotcheruyot@gmail.com>
2018-08-24 10:52:18 +0300

    added the second class and its test file

--- userAccounts.py
+++ userAccounts.py
@@ -2,14 +2,17 @@ class UserAccounts:
     """
     Class that generates new instances of user accounts
     """
+    users_list = [] # Empty users list
 
     def __init__(self, account_name, account_password):
         '''
         __init__ method that helps us define properties for our objects.
-
-
         '''
 
-    self.account_name = account_name
-    self.account_password = account_password
+        self.account_name = account_name
+        self.account_password = account_password
+
+
+
+
 
