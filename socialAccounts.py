e83510f973842086ad57a0d3bd86c84bbe73e4cf
cherucole <cheruyotcheruyot@gmail.com>
2018-08-24 11:54:19 +0300

    added test to ensure it saves multiple accounts

--- socialAccounts.py
+++ socialAccounts.py
@@ -4,13 +4,14 @@ class SocialAccounts:
         """
     social_accounts_list = []  # Empty users list
 
-    def __init__(self, social_account_name, social_account_password):
+    def __init__(self, social_account_name, social_account_password, password_length):
         '''
         __init__ method that helps us define properties for our objects.
         '''
 
         self.social_account_name = social_account_name
         self.social_account_password = social_account_password
+        self.password_length= password_length
 
     def save_social_account(self):
         '''
--- test_socialAccounts.py
+++ test_socialAccounts.py
@@ -5,12 +5,14 @@ class TestSocialAccounts(unittest.TestCase):
     '''
     Test class that we define and test methods to use in the SocialAccount class
     '''
+    def tearDown(self):
+        SocialAccounts.social_accounts_list=[]
 
     def setUp(self):
         '''
         this setup method is run before each test case is executed
         '''
-        self.new_social_account= SocialAccounts("twitter", "twitterPassword") #creating the social account object
+        self.new_social_account= SocialAccounts("twitter", "twitterPassword", "15") #creating the social account object
 
     def test_init(self):
         '''
@@ -18,6 +20,7 @@ class TestSocialAccounts(unittest.TestCase):
         '''
         self.assertEqual(self.new_social_account.social_account_name, "twitter")
         self.assertEqual(self.new_social_account.social_account_password,"twitterPassword")
+        self.assertEqual(self.new_social_account.password_length,"15")
 
 
     def test_save_social_account(self):
@@ -26,6 +29,15 @@ class TestSocialAccounts(unittest.TestCase):
         '''
         self.new_social_account.save_social_account() #saving the social account
         self.assertEqual(len(SocialAccounts.social_accounts_list),1)
+
+    def test_save_multiple_social_account(self):
+        '''
+        test to check if we can save multiple social account details
+        '''
+        self.new_social_account.save_social_account()
+        test_social_account = SocialAccounts("musical.ly", "passmusic", "9") #new account to save
+        test_social_account.save_social_account()
+        self.assertEqual(len(SocialAccounts.social_accounts_list),2)
 
 
 if __name__=='__main__':
