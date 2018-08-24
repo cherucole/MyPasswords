5f7c0f245b8ece61b79908290548e51e734e0728
cherucole <cheruyotcheruyot@gmail.com>
2018-08-24 11:38:37 +0300

    added option to save accounts

--- socialAccounts.py
+++ socialAccounts.py
@@ -2,7 +2,7 @@ class SocialAccounts:
     """
         Class that generates new instances of user accounts
         """
-    social_list = []  # Empty users list
+    social_accounts_list = []  # Empty users list
 
     def __init__(self, social_account_name, social_account_password):
         '''
@@ -11,3 +11,9 @@ class SocialAccounts:
 
         self.social_account_name = social_account_name
         self.social_account_password = social_account_password
+
+    def save_social_account(self):
+        '''
+        creating the method that saves social accounts
+        '''
+        SocialAccounts.social_accounts_list.append(self)
--- test_socialAccounts.py
+++ test_socialAccounts.py
@@ -20,5 +20,13 @@ class TestSocialAccounts(unittest.TestCase):
         self.assertEqual(self.new_social_account.social_account_password,"twitterPassword")
 
 
+    def test_save_social_account(self):
+        '''
+        test case to see if the social account details are being saved to the list
+        '''
+        self.new_social_account.save_social_account() #saving the social account
+        self.assertEqual(len(SocialAccounts.social_accounts_list),1)
+
+
 if __name__=='__main__':
     unittest.main()
