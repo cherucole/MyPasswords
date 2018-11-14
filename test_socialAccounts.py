d38db015703dc828118dc44bfaa3e6ae3e96f489
cherucole <cheruyotcheruyot@gmail.com>
2018-11-14 22:48:17 +0300

    Formatting code

--- test_socialAccounts.py
+++ test_socialAccounts.py
@@ -84,15 +84,5 @@ class TestSocialAccounts(unittest.TestCase):
         self.assertEqual(SocialAccounts.display_social_accounts(), SocialAccounts.social_accounts_list)
 
 
-
-    # def test_copy_username(self):
-    #     '''
-    #     Test to confirm we are copying the username from a found account
-    #     '''
-    #     self.new_social_account.save_social_account()
-    #     SocialAccounts.copy_username("twitter")
-    #
-    #     self.assertEqual(self.new_social_account.social_account_username,pyperclip.paste())
-
 if __name__=='__main__':
     unittest.main()
